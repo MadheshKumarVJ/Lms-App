84d3a5662db44539f7825ce8cd3fc95567e9b43c
Maddy <vjmadheshkumarofficial@gmail.com>
2022-12-18 09:56:28 +0530

    Reformat files

--- educa/tests/courses/test_model_mixin.py
+++ educa/tests/courses/test_model_mixin.py
@@ -1,7 +1,17 @@
 from django.test import TestCase
-from courses.models import Subject, Course, Module, Content, Text, Image, Video, File
-from tests.factories import UserFactory 
+from courses.models import (
+    Subject,
+    Course,
+    Module,
+    Content,
+    Text,
+    Image,
+    Video,
+    File,
+)
+from tests.factories import UserFactory
 from django.core.files.uploadedfile import SimpleUploadedFile
+
 
 class Modelmixin(TestCase):
     def setUp(self):
@@ -17,7 +27,12 @@ class Modelmixin(TestCase):
             title="course1",
             slug="course1",
         )
-        self.image = SimpleUploadedFile(name='sample.jpg', content=open("test_files/sample.jpg", 'rb').read(), content_type='image/jpeg')
+        self.image = SimpleUploadedFile(
+            name="sample.jpg",
+            content=open("test_files/sample.jpg", "rb").read(),
+            content_type="image/jpeg",
+        )
+
     def create_modules(self, count, order_number=None):
         for _ in range(count):
             Module.objects.create(
@@ -39,37 +54,41 @@ class Modelmixin(TestCase):
                 slug="course1",
             )
         return Course.objects.all()
-    def create_module_content(self,content,module=None):
+
+    def create_module_content(self, content, module=None):
         if module is None:
             module = self.create_modules(1).first()
         Content.objects.create(
-            module = module,
-            item = content,
+            module=module,
+            item=content,
         )
         return Content.objects.first()
+
     def create_text(self):
-        sample_text =Text.objects.create(
-            content = "test",
-            title = "test_title",
-            owner = self.user)
-        return sample_text
-    def create_image(self):
-        sample_image =Image.objects.create(
-            file  = self.image,
-            title = "sample_img",
-            owner = self.user)
-        return sample_image
-    def create_video_url(self):
-        sample_video =Video.objects.create(
-            url  = "https://youtu.be/V8-ikp2dLvY",
-            title = "sample_img",
-            owner = self.user)
-        return sample_video
-    def create_file(self):
-     with open('test_files/myfile.txt', 'r',encoding = 'utf-8') as file:  
-        sample_file =File.objects.create(
-            file = file.read(),
-            title = "TestFile",
-            owner = self.user,
+        sample_text = Text.objects.create(
+            content="test", title="test_title", owner=self.user
         )
-        return sample_file
+        return sample_text
+
+    def create_image(self):
+        sample_image = Image.objects.create(
+            file=self.image, title="sample_img", owner=self.user
+        )
+        return sample_image
+
+    def create_video_url(self):
+        sample_video = Video.objects.create(
+            url="https://youtu.be/V8-ikp2dLvY",
+            title="sample_img",
+            owner=self.user,
+        )
+        return sample_video
+
+    def create_file(self):
+        with open("test_files/myfile.txt", "r", encoding="utf-8") as file:
+            sample_file = File.objects.create(
+                file=file.read(),
+                title="TestFile",
+                owner=self.user,
+            )
+            return sample_file
--- educa/tests/courses/test_views.py
+++ educa/tests/courses/test_views.py
@@ -3,7 +3,6 @@ from django.urls import reverse
 from tests.courses.test_model_mixin import Modelmixin
 
 
-
 class TestListView(Modelmixin, TestCase):
     def test_course_list_view_redirects_to_login_page(self):
         response = self.client.get((reverse("course:manage_course_list")))
@@ -40,24 +39,32 @@ class TestListView(Modelmixin, TestCase):
         )
         self.assertTemplateUsed(response, "courses/manage/module/formset.html")
 
-    def test_text_content_get_stored_in_module_contents(self): 
+    def test_module_content_list_template_used(self):
+        self.create_modules(1)
+        self.client.login(username="maddy", password="123")
+        response = self.client.get(
+            reverse("course:module_content_list", args=[self.course1.pk])
+        )
+        self.assertTemplateUsed(
+            response, "courses/manage/module/content_list.html"
+        )
+
+    def test_text_content_get_stored_in_module_contents(self):
         text_content = self.create_text()
         module_content = self.create_module_content(content=text_content)
-        self.assertEqual(text_content ,module_content.item)
-    
-    def test_image_content_get_stored_in_module_contents(self): 
+        self.assertEqual(text_content, module_content.item)
+
+    def test_image_content_get_stored_in_module_contents(self):
         image_content = self.create_image()
         module_content = self.create_module_content(content=image_content)
-        self.assertEqual(image_content ,module_content.item)
-        
-    def test_video_content_get_stored_in_module_contents(self): 
+        self.assertEqual(image_content, module_content.item)
+
+    def test_video_content_get_stored_in_module_contents(self):
         video_content = self.create_video_url()
         module_content = self.create_module_content(content=video_content)
-        self.assertEqual(video_content ,module_content.item)
-    
-    def test_file_content_get_stored_in_module_contents(self): 
+        self.assertEqual(video_content, module_content.item)
+
+    def test_file_content_get_stored_in_module_contents(self):
         file_content = self.create_file()
         module_content = self.create_module_content(content=file_content)
-        self.assertEqual(file_content ,module_content.item)
-    
-
+        self.assertEqual(file_content, module_content.item)
