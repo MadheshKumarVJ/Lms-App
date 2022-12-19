9bea442b7f18453badf310aad4b6fb7318195dcc
MadheshKumarVJ <vjmadhesh@testpress.in>
2022-12-19 17:21:23 +0530

    Add support to edit contents in module

--- educa/courses/urls.py
+++ educa/courses/urls.py
@@ -32,4 +32,9 @@ urlpatterns = [
         views.ContentDeleteView.as_view(),
         name="module_content_delete",
     ),
+    path(
+        "module/<int:module_id>/content/<model_name>/<id>/",
+        views.ContentCreateUpdateView.as_view(),
+        name="module_content_update",
+    ),
 ]
